18415021317320f0db122959fed8978c3759627d
适然 <sauntor@yeah.net>
2017-03-14 13:55:51 +0800

    add a shell script for cleaning up  project config files generated by kdevelop and CMake

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf ./Makefile 
+rm -rf ./.ycm_extra_conf.py
+rm -rf ./cmake_install.cmake
+rm -rf ./CMakeFiles
+rm -rf ./CMakeCache.txt
+rm -rf ./lcmysql-demo.kdev4
+rm -rf ./compile_commands.json
+rm -rf ./build
+rm -rf ./.kdev4
