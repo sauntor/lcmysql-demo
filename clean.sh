b33a93f9d59b4bd9c1e3869ade37ff259bb8819b
Sauntor <sauntor@SuSE.os>
2017-03-18 20:19:26 +0800

    Make lcmysql reference to upstream master

--- clean.sh
+++ clean.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
-#make clean
-rm -rf ./Makefile 
-rm -rf ./.ycm_extra_conf.py
-rm -rf ./cmake_install.cmake
-rm -rf ./CMakeFiles
-rm -rf ./CMakeCache.txt
-rm -rf ./lcmysql-demo.kdev4
-rm -rf ./compile_commands.json
-rm -rf ./build
-rm -rf ./.kdev4
+pwd=$(cd `dirname $0`; pwd)
+[[ -f $pwd/Makefile ]] && make -f $pwd/Makefile clean
+rm -rf $pwd/Makefile 
+rm -rf $pwd/.ycm_extra_conf.py
+rm -rf $pwd/cmake_install.cmake
+rm -rf $pwd/CMakeFiles
+rm -rf $pwd/CMakeCache.txt
+rm -rf $pwd/lcmysql-demo.kdev4
+rm -rf $pwd/compile_commands.json
+rm -rf $pwd/build
+rm -rf $pwd/.kdev4
